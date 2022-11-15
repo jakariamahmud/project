a2202aa965d0965d75c53b7f29915f95932faeef
jakaria-mahmud <jakaria0499@gmail.com>
2022-11-15 14:14:34 +0600

    portfolio and portfolio-cryptical is added

---
+++ portfolio-cryptical/js/script.js
@@ -0,0 +1,7 @@
+// typeing animation
+let typed = new Typed(".typing", {
+  strings:[" ..... ", "Web Designer", "App Developer", "Web Developer", "Youtuber"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+})
